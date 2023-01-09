6a088c79ae3610f9415158ea44a7b77be78a8cb5
Eugenia <mbp@MacBook-Pro-MBP.Dlink>
2023-01-09 17:30:36 +0300

    create and edit index.js

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import readlineSync from 'readline-sync';
+
+const gameLogic = (rule, question, rightAnswer) => {
+  console.log('Welcome to the Brain Games!');
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
+  console.log(rule);
+  for (let i = 0; i < 3; i += 1) {
+    console.log(`Question: ${question}`);
+    const playerAnswer = readlineSync.question('Your answer: ');
+    if (playerAnswer === rightAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${playerName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${playerName}!`);
+};
+
+export default gameLogic;

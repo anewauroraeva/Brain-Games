d39f6e98e4105a1e1bf71a352841dbefad63ada7
Eugenia <mbp@MacBook-Pro-MBP.Dlink>
2023-01-10 16:32:43 +0300

    edit and add callback function to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import readlineSync from 'readline-sync';
 
-const gameLogic = (rule, question, rightAnswer) => {
+const gameLogic = (rule, callback) => {
   console.log('Welcome to the Brain Games!');
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
   console.log(rule);
   for (let i = 0; i < 3; i += 1) {
+    const [question, rightAnswer] = callback();
     console.log(`Question: ${question}`);
     const playerAnswer = readlineSync.question('Your answer: ');
     if (playerAnswer === rightAnswer) {

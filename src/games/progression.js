1f1927fd80e289a86c50b56c093f0874a2b0d0a5
Eugenia <mbp@MacBook-Pro-MBP.Dlink>
2023-01-16 19:51:27 +0300

    add and edit progression.js

---
+++ src/games/progression.js
@@ -0,0 +1,31 @@
+import gameLogic from '../index.js';
+import getRandomValue from '../getRandomValue.js';
+
+const rule = 'What number is missing in the progression?';
+
+const getProgressionArray = (startNum, stepOfProgression, lengthOfProgression) => {
+  const progressionArray = [];
+  for (let i = 0; i < lengthOfProgression; i += 1) {
+    const nextNum = startNum + i * stepOfProgression;
+    progressionArray.push(nextNum);
+  }
+  return progressionArray;
+};
+
+const progressionGame = () => {
+  const startNum = getRandomValue(1, 100);
+  const stepOfProgression = getRandomValue(1, 10);
+  const lengthOfProgression = getRandomValue(5, 10);
+  const progression = getProgressionArray(startNum, stepOfProgression, lengthOfProgression);
+  const randomIndex = getRandomValue(0, lengthOfProgression);
+  const rightAnswer = progression[randomIndex].toString();
+  progression[randomIndex] = '..';
+  const question = progression.join(' ');
+  return [question, rightAnswer];
+};
+
+const runProgressionGame = () => {
+  gameLogic(rule, progressionGame);
+};
+
+export default runProgressionGame;

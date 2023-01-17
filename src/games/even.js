d1ca47d856e8c882a78791cc01c60ca3dc9c5550
Eugenia <mbp@MacBook-Pro-MBP.Dlink>
2023-01-17 19:01:41 +0300

    increase the difficulty in even game

--- src/games/even.js
+++ src/games/even.js
@@ -5,7 +5,7 @@ const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (questionValue) => questionValue % 2 === 0;
 
 const evenGame = () => {
-  const questionValue = getRandomValue(1, 50);
+  const questionValue = getRandomValue(1, 100);
   const question = `${questionValue}`;
   const rightAnswer = (isEven(questionValue) === true ? 'yes' : 'no');
   return [question, rightAnswer];

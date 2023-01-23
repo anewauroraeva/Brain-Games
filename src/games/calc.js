f7a3ec8358df68841f7ac5d15486b23ebfcab045
Eugenia <mbp@MacBook-Pro-MBP.Dlink>
2023-01-23 18:06:08 +0300

    take out getRightAnswer and remove Hung. notation in calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,25 +3,26 @@ import getRandomValue from '../getRandomValue.js';
 
 const rule = 'What is the result of the expression?';
 
+const getRightAnswer = (firstOperand, randomOperator, secondOperand) => {
+  switch (randomOperator) {
+    case '+':
+      return firstOperand + secondOperand;
+    case '-':
+      return firstOperand - secondOperand;
+    case '*':
+      return firstOperand * secondOperand;
+    default:
+      return "there's an error";
+  }
+};
+
 const generateCalcGame = () => {
   const firstOperand = getRandomValue(1, 25);
   const secondOperand = getRandomValue(1, 10);
-  const operatorsArray = ['+', '-', '*'];
-  const getRandomOperator = operatorsArray[Math.floor(Math.random() * operatorsArray.length)];
-  const question = `${firstOperand} ${getRandomOperator} ${secondOperand}`;
-  const neededAnswer = () => {
-    switch (getRandomOperator) {
-      case '+':
-        return firstOperand + secondOperand;
-      case '-':
-        return firstOperand - secondOperand;
-      case '*':
-        return firstOperand * secondOperand;
-      default:
-        return "there's an error";
-    }
-  };
-  const rightAnswer = neededAnswer(firstOperand, getRandomOperator, secondOperand).toString();
+  const operators = ['+', '-', '*'];
+  const randomOperator = operators[Math.floor(Math.random() * operators.length)];
+  const question = `${firstOperand} ${randomOperator} ${secondOperand}`;
+  const rightAnswer = getRightAnswer(firstOperand, randomOperator, secondOperand).toString();
   return [question, rightAnswer];
 };
 

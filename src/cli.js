099972047370318d5b1bcc6c4de41a7e4f1a32b4
Eugenia <mbp@MacBook-Pro-MBP.Dlink>
2023-01-23 18:00:29 +0300

    remove userName from params in cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const greeting = (userName) => {
-  // eslint-disable-next-line no-param-reassign
-  userName = readlineSync.question('May I have your name? ');
+const greeting = () => {
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
 export default greeting;
